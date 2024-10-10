ea41259393cc970001b0f678a4dea31664fc3590
MahagaNajwanKaidan <mahaga070730@gmail.com>
2024-10-10 12:16:11 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FE-MahagaNajwanKaidan/FE_MahagaNajwanKaidan_XIIRPL',
+  base: '/FE-MahagaNajwanKaidan/',
   plugins: [react()],
 })
